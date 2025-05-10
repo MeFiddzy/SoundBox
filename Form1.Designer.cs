cb81d16d7959d05ce56fade8d3be7abd06aed93f
MeFiddzy <fiddzy.pg@gmail.com>
2025-05-10 12:52:10 +0300

    Features

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,14 +48,21 @@
             buttonrRe = new Button();
             buttonUp = new Button();
             buttonOp = new Button();
+            b20 = new Button();
+            b19 = new Button();
+            b18 = new Button();
+            b17 = new Button();
+            labelS = new Label();
+            labelSet = new Label();
             SuspendLayout();
             // 
             // b1
             // 
             b1.Font = new Font("Segoe UI", 20F);
-            b1.Location = new Point(54, 57);
+            b1.Location = new Point(77, 99);
+            b1.Margin = new Padding(4, 5, 4, 5);
             b1.Name = "b1";
-            b1.Size = new Size(217, 92);
+            b1.Size = new Size(310, 153);
             b1.TabIndex = 0;
             b1.Text = "button1";
             b1.UseVisualStyleBackColor = true;
@@ -64,9 +71,10 @@
             // b2
             // 
             b2.Font = new Font("Segoe UI", 20F);
-            b2.Location = new Point(289, 57);
+            b2.Location = new Point(413, 99);
+            b2.Margin = new Padding(4, 5, 4, 5);
             b2.Name = "b2";
-            b2.Size = new Size(217, 92);
+            b2.Size = new Size(310, 153);
             b2.TabIndex = 1;
             b2.Text = "button1";
             b2.UseVisualStyleBackColor = true;
@@ -75,9 +83,10 @@
             // b3
             // 
             b3.Font = new Font("Segoe UI", 20F);
-            b3.Location = new Point(531, 57);
+            b3.Location = new Point(759, 99);
+            b3.Margin = new Padding(4, 5, 4, 5);
             b3.Name = "b3";
-            b3.Size = new Size(217, 92);
+            b3.Size = new Size(310, 153);
             b3.TabIndex = 2;
             b3.Text = "button1";
             b3.UseVisualStyleBackColor = true;
@@ -86,9 +95,10 @@
             // b4
             // 
             b4.Font = new Font("Segoe UI", 20F);
-            b4.Location = new Point(767, 57);
+            b4.Location = new Point(1096, 99);
+            b4.Margin = new Padding(4, 5, 4, 5);
             b4.Name = "b4";
-            b4.Size = new Size(217, 92);
+            b4.Size = new Size(310, 153);
             b4.TabIndex = 3;
             b4.Text = "button1";
             b4.UseVisualStyleBackColor = true;
@@ -97,9 +107,10 @@
             // b8
             // 
             b8.Font = new Font("Segoe UI", 20F);
-            b8.Location = new Point(767, 155);
+            b8.Location = new Point(1096, 262);
+            b8.Margin = new Padding(4, 5, 4, 5);
             b8.Name = "b8";
-            b8.Size = new Size(217, 92);
+            b8.Size = new Size(310, 153);
             b8.TabIndex = 7;
             b8.Text = "button1";
             b8.UseVisualStyleBackColor = true;
@@ -108,9 +119,10 @@
             // b7
             // 
             b7.Font = new Font("Segoe UI", 20F);
-            b7.Location = new Point(531, 155);
+            b7.Location = new Point(759, 262);
+            b7.Margin = new Padding(4, 5, 4, 5);
             b7.Name = "b7";
-            b7.Size = new Size(217, 92);
+            b7.Size = new Size(310, 153);
             b7.TabIndex = 6;
             b7.Text = "button1";
             b7.UseVisualStyleBackColor = true;
@@ -119,9 +131,10 @@
             // b6
             // 
             b6.Font = new Font("Segoe UI", 20F);
-            b6.Location = new Point(289, 155);
+            b6.Location = new Point(413, 262);
+            b6.Margin = new Padding(4, 5, 4, 5);
             b6.Name = "b6";
-            b6.Size = new Size(217, 92);
+            b6.Size = new Size(310, 153);
             b6.TabIndex = 5;
             b6.Text = "button6";
             b6.UseVisualStyleBackColor = true;
@@ -130,9 +143,10 @@
             // b5
             // 
             b5.Font = new Font("Segoe UI", 20F);
-            b5.Location = new Point(54, 155);
+            b5.Location = new Point(77, 262);
+            b5.Margin = new Padding(4, 5, 4, 5);
             b5.Name = "b5";
-            b5.Size = new Size(217, 92);
+            b5.Size = new Size(310, 153);
             b5.TabIndex = 4;
             b5.Text = "button1";
             b5.UseVisualStyleBackColor = true;
@@ -141,9 +155,10 @@
             // b12
             // 
             b12.Font = new Font("Segoe UI", 20F);
-            b12.Location = new Point(767, 253);
+            b12.Location = new Point(1096, 426);
+            b12.Margin = new Padding(4, 5, 4, 5);
             b12.Name = "b12";
-            b12.Size = new Size(217, 92);
+            b12.Size = new Size(310, 153);
             b12.TabIndex = 11;
             b12.Text = "button1";
             b12.UseVisualStyleBackColor = true;
@@ -152,9 +167,10 @@
             // b11
             // 
             b11.Font = new Font("Segoe UI", 20F);
-            b11.Location = new Point(531, 253);
+            b11.Location = new Point(759, 426);
+            b11.Margin = new Padding(4, 5, 4, 5);
             b11.Name = "b11";
-            b11.Size = new Size(217, 92);
+            b11.Size = new Size(310, 153);
             b11.TabIndex = 10;
             b11.Text = "button1";
             b11.UseVisualStyleBackColor = true;
@@ -163,9 +179,10 @@
             // b10
             // 
             b10.Font = new Font("Segoe UI", 20F);
-            b10.Location = new Point(289, 253);
+            b10.Location = new Point(413, 426);
+            b10.Margin = new Padding(4, 5, 4, 5);
             b10.Name = "b10";
-            b10.Size = new Size(217, 92);
+            b10.Size = new Size(310, 153);
             b10.TabIndex = 9;
             b10.Text = "button10";
             b10.UseVisualStyleBackColor = true;
@@ -174,9 +191,10 @@
             // b9
             // 
             b9.Font = new Font("Segoe UI", 20F);
-            b9.Location = new Point(54, 253);
+            b9.Location = new Point(77, 426);
+            b9.Margin = new Padding(4, 5, 4, 5);
             b9.Name = "b9";
-            b9.Size = new Size(217, 92);
+            b9.Size = new Size(310, 153);
             b9.TabIndex = 8;
             b9.Text = "button1";
             b9.UseVisualStyleBackColor = true;
@@ -185,9 +203,10 @@
             // b16
             // 
             b16.Font = new Font("Segoe UI", 20F);
-            b16.Location = new Point(767, 351);
+            b16.Location = new Point(1096, 589);
+            b16.Margin = new Padding(4, 5, 4, 5);
             b16.Name = "b16";
-            b16.Size = new Size(217, 92);
+            b16.Size = new Size(310, 153);
             b16.TabIndex = 15;
             b16.Text = "button1";
             b16.UseVisualStyleBackColor = true;
@@ -196,9 +215,10 @@
             // b15
             // 
             b15.Font = new Font("Segoe UI", 20F);
-            b15.Location = new Point(531, 351);
+            b15.Location = new Point(759, 589);
+            b15.Margin = new Padding(4, 5, 4, 5);
             b15.Name = "b15";
-            b15.Size = new Size(217, 92);
+            b15.Size = new Size(310, 153);
             b15.TabIndex = 14;
             b15.Text = "button1";
             b15.UseVisualStyleBackColor = true;
@@ -207,9 +227,10 @@
             // b14
             // 
             b14.Font = new Font("Segoe UI", 20F);
-            b14.Location = new Point(289, 351);
+            b14.Location = new Point(413, 589);
+            b14.Margin = new Padding(4, 5, 4, 5);
             b14.Name = "b14";
-            b14.Size = new Size(217, 92);
+            b14.Size = new Size(310, 153);
             b14.TabIndex = 13;
             b14.Text = "button14";
             b14.UseVisualStyleBackColor = true;
@@ -218,9 +239,10 @@
             // b13
             // 
             b13.Font = new Font("Segoe UI", 20F);
-            b13.Location = new Point(54, 351);
+            b13.Location = new Point(77, 589);
+            b13.Margin = new Padding(4, 5, 4, 5);
             b13.Name = "b13";
-            b13.Size = new Size(217, 92);
+            b13.Size = new Size(310, 153);
             b13.TabIndex = 12;
             b13.Text = "button1";
             b13.UseVisualStyleBackColor = true;
@@ -229,9 +251,10 @@
             // buttonS
             // 
             buttonS.Font = new Font("Segoe UI", 20F);
-            buttonS.Location = new Point(767, 486);
+            buttonS.Location = new Point(1096, 1016);
+            buttonS.Margin = new Padding(4, 5, 4, 5);
             buttonS.Name = "buttonS";
-            buttonS.Size = new Size(217, 92);
+            buttonS.Size = new Size(310, 120);
             buttonS.TabIndex = 16;
             buttonS.Text = "Stop Sound";
             buttonS.UseVisualStyleBackColor = true;
@@ -240,9 +263,10 @@
             // buttonrRe
             // 
             buttonrRe.Font = new Font("Segoe UI", 20F);
-            buttonrRe.Location = new Point(525, 486);
+            buttonrRe.Location = new Point(759, 1016);
+            buttonrRe.Margin = new Padding(4, 5, 4, 5);
             buttonrRe.Name = "buttonrRe";
-            buttonrRe.Size = new Size(217, 92);
+            buttonrRe.Size = new Size(310, 120);
             buttonrRe.TabIndex = 17;
             buttonrRe.Text = "Reload Soundboard";
             buttonrRe.UseVisualStyleBackColor = true;
@@ -251,30 +275,106 @@
             // buttonUp
             // 
             buttonUp.Font = new Font("Segoe UI", 20F);
-            buttonUp.Location = new Point(278, 486);
+            buttonUp.Location = new Point(413, 1016);
+            buttonUp.Margin = new Padding(4, 5, 4, 5);
             buttonUp.Name = "buttonUp";
-            buttonUp.Size = new Size(217, 92);
+            buttonUp.Size = new Size(310, 120);
             buttonUp.TabIndex = 18;
-            buttonUp.Text = "Upload files";
+            buttonUp.Text = "Upload sounds";
             buttonUp.UseVisualStyleBackColor = true;
             buttonUp.Click += buttonUp_Click;
             // 
             // buttonOp
             // 
             buttonOp.Font = new Font("Segoe UI", 20F);
-            buttonOp.Location = new Point(54, 486);
+            buttonOp.Location = new Point(77, 1016);
+            buttonOp.Margin = new Padding(4, 5, 4, 5);
             buttonOp.Name = "buttonOp";
-            buttonOp.Size = new Size(217, 92);
+            buttonOp.Size = new Size(310, 120);
             buttonOp.TabIndex = 19;
             buttonOp.Text = "Open sound folder";
             buttonOp.UseVisualStyleBackColor = true;
             buttonOp.Click += buttonOp_Click;
             // 
+            // b20
+            // 
+            b20.Font = new Font("Segoe UI", 20F);
+            b20.Location = new Point(1096, 752);
+            b20.Margin = new Padding(4, 5, 4, 5);
+            b20.Name = "b20";
+            b20.Size = new Size(310, 153);
+            b20.TabIndex = 23;
+            b20.Text = "button1";
+            b20.UseVisualStyleBackColor = true;
+            b20.Click += b20_Click;
+            // 
+            // b19
+            // 
+            b19.Font = new Font("Segoe UI", 20F);
+            b19.Location = new Point(759, 752);
+            b19.Margin = new Padding(4, 5, 4, 5);
+            b19.Name = "b19";
+            b19.Size = new Size(310, 153);
+            b19.TabIndex = 22;
+            b19.Text = "button1";
+            b19.UseVisualStyleBackColor = true;
+            b19.Click += b19_Click;
+            // 
+            // b18
+            // 
+            b18.Font = new Font("Segoe UI", 20F);
+            b18.Location = new Point(413, 752);
+            b18.Margin = new Padding(4, 5, 4, 5);
+            b18.Name = "b18";
+            b18.Size = new Size(310, 153);
+            b18.TabIndex = 21;
+            b18.Text = "button14";
+            b18.UseVisualStyleBackColor = true;
+            b18.Click += b18_Click;
+            // 
+            // b17
+            // 
+            b17.Font = new Font("Segoe UI", 20F);
+            b17.Location = new Point(77, 752);
+            b17.Margin = new Padding(4, 5, 4, 5);
+            b17.Name = "b17";
+            b17.Size = new Size(310, 153);
+            b17.TabIndex = 20;
+            b17.Text = "button1";
+            b17.UseVisualStyleBackColor = true;
+            b17.Click += b17_Click;
+            // 
+            // labelS
+            // 
+            labelS.AutoSize = true;
+            labelS.Font = new Font("Segoe UI", 30F);
+            labelS.Location = new Point(635, 1);
+            labelS.Name = "labelS";
+            labelS.Size = new Size(243, 81);
+            labelS.TabIndex = 24;
+            labelS.Text = "Sounds:";
+            // 
+            // labelSet
+            // 
+            labelSet.AutoSize = true;
+            labelSet.Font = new Font("Segoe UI", 30F);
+            labelSet.Location = new Point(625, 930);
+            labelSet.Name = "labelSet";
+            labelSet.Size = new Size(260, 81);
+            labelSet.TabIndex = 25;
+            labelSet.Text = "Settings:";
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1026, 618);
+            ClientSize = new Size(1466, 1150);
+            Controls.Add(labelSet);
+            Controls.Add(labelS);
+            Controls.Add(b20);
+            Controls.Add(b19);
+            Controls.Add(b18);
+            Controls.Add(b17);
             Controls.Add(buttonOp);
             Controls.Add(buttonUp);
             Controls.Add(buttonrRe);
@@ -295,9 +395,11 @@
             Controls.Add(b3);
             Controls.Add(b2);
             Controls.Add(b1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -322,5 +424,11 @@
         private Button buttonrRe;
         private Button buttonUp;
         private Button buttonOp;
+        private Button b20;
+        private Button b19;
+        private Button b18;
+        private Button b17;
+        private Label labelS;
+        private Label labelSet;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Media;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace Soundbox
@@ -17,26 +18,44 @@ namespace Soundbox
         {
             InitializeComponent();
             reloadS();
+
+            if (sounds.Length == 0)
+            {
+                MessageBox.Show("Warning: There are no .wav files in the Sounds folder.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
 
         public void reloadS()
         {
-            sounds = Directory.GetFiles(Path.Combine(path, "Sounds"), "*.wav");
-
-            if (sounds.Length == 0)
+            try
             {
-                MessageBox.Show("Error: There are no .wav files in the Sounds folder.");
+                sounds = Directory.GetFiles(Path.Combine(path, "Sounds"), "*.wav");
+            }
+            catch
+            {
+                MessageBox.Show("Error: The Sounds folder has been deleted!", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 Close();
                 return;
             }
 
-            Button[] buttons = { b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16 };
+            if (sounds.Length == 20)
+            {
+                buttonUp.Enabled = false;
+            }
+            else
+            {
+                
+                buttonUp.Enabled= true;
+            }
+
+            Button[] buttons = { b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20 };
 
             for (int i = 0; i < buttons.Length; i++)
             {
                 if (i < sounds.Length)
                 {
                     string fileName = Path.GetFileNameWithoutExtension(sounds[i]);
+                    fileName = fileName.IndexOf('`') != -1 ? fileName.Substring(fileName.IndexOf('`') + 1) : fileName;
                     buttons[i].Enabled = true;
                     buttons[i].Text = fileName;
                 }
@@ -57,7 +76,7 @@ namespace Soundbox
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error playing sound: {ex.Message}");
+                MessageBox.Show("Warning: Tried playing an unexisting sound.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
@@ -71,7 +90,7 @@ namespace Soundbox
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show($"Error stopping sound: {ex.Message}");
+                    MessageBox.Show("Warning: tried to stop an unexisting sound", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
             }
 
@@ -91,26 +110,8 @@ namespace Soundbox
                 }
             }
 
-            MessageBox.Show($"No sound file found for {buttonText}");
-        }
-
-        private void b1_Click(object sender, EventArgs e) => playButtonSound(b1);
-        private void b2_Click(object sender, EventArgs e) => playButtonSound(b2);
-        private void b3_Click(object sender, EventArgs e) => playButtonSound(b3);
-        private void b4_Click(object sender, EventArgs e) => playButtonSound(b4);
-        private void b5_Click(object sender, EventArgs e) => playButtonSound(b5);
-        private void b6_Click(object sender, EventArgs e) => playButtonSound(b6);
-        private void b7_Click(object sender, EventArgs e) => playButtonSound(b7);
-        private void b8_Click(object sender, EventArgs e) => playButtonSound(b8);
-        private void b9_Click(object sender, EventArgs e) => playButtonSound(b9);
-        private void b10_Click(object sender, EventArgs e) => playButtonSound(b10);
-        private void b11_Click(object sender, EventArgs e) => playButtonSound(b11);
-        private void b12_Click(object sender, EventArgs e) => playButtonSound(b12);
-        private void b13_Click(object sender, EventArgs e) => playButtonSound(b13);
-        private void b14_Click(object sender, EventArgs e) => playButtonSound(b14);
-        private void b15_Click(object sender, EventArgs e) => playButtonSound(b15);
-        private void b16_Click(object sender, EventArgs e) => playButtonSound(b16);
-
+            MessageBox.Show("Warning: No sound file found.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }     
 
         private void buttonS_Click(object sender, EventArgs e)
         {
@@ -150,7 +151,8 @@ namespace Soundbox
 
             if (!Directory.Exists(folderPath))
             {
-                MessageBox.Show("The Sounds has been deleted.");
+                MessageBox.Show("Error: The Sounds has been deleted.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
                 return;
             }
 
@@ -160,8 +162,30 @@ namespace Soundbox
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error opening folder: {ex.Message}");
+                MessageBox.Show("Error: The Sounds has been deleted.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+                return;
             }
         }
+        private void b1_Click(object sender, EventArgs e) => playButtonSound(b1);
+        private void b2_Click(object sender, EventArgs e) => playButtonSound(b2);
+        private void b3_Click(object sender, EventArgs e) => playButtonSound(b3);
+        private void b4_Click(object sender, EventArgs e) => playButtonSound(b4);
+        private void b5_Click(object sender, EventArgs e) => playButtonSound(b5);
+        private void b6_Click(object sender, EventArgs e) => playButtonSound(b6);
+        private void b7_Click(object sender, EventArgs e) => playButtonSound(b7);
+        private void b8_Click(object sender, EventArgs e) => playButtonSound(b8);
+        private void b9_Click(object sender, EventArgs e) => playButtonSound(b9);
+        private void b10_Click(object sender, EventArgs e) => playButtonSound(b10);
+        private void b11_Click(object sender, EventArgs e) => playButtonSound(b11);
+        private void b12_Click(object sender, EventArgs e) => playButtonSound(b12);
+        private void b13_Click(object sender, EventArgs e) => playButtonSound(b13);
+        private void b14_Click(object sender, EventArgs e) => playButtonSound(b14);
+        private void b15_Click(object sender, EventArgs e) => playButtonSound(b15);
+        private void b16_Click(object sender, EventArgs e) => playButtonSound(b16);
+        private void b17_Click(object sender, EventArgs e) => playButtonSound(b17);
+        private void b18_Click(object sender, EventArgs e) => playButtonSound(b18);
+        private void b19_Click(object sender, EventArgs e) => playButtonSound(b19);
+        private void b20_Click(object sender, EventArgs e) => playButtonSound(b20);
     }
 }
